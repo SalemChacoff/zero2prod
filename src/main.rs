0ea18dfe72ad9e272e4b751bc798d59ee9dd5a24
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2024-11-05 22:38:37 -0300

    Add First Integration Tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+
+#[tokio::main]
+async fn main() {
+	let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+	// We retrieve the port assigned to us by the OS.
+	let port = listener.local_addr().unwrap().port();
+	let server = run(listener).expect("Failed to bind address");
+	let _ = tokio::spawn(server);
+	// We return the application port to the caller.
+	let finalport = format!("http://127.0.0.1:{}", port);
+    println!("Server running on {}", finalport);
 }
