e85764d1c006fe2fd80863f75b6f49093051944d
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2024-11-07 18:38:48 -0300

    Configure And Connect To DB

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-//! main.rs
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-
-use zero2prod::run;
-
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
-async fn main() {
-	let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-	// We retrieve the port assigned to us by the OS.
-	let port = listener.local_addr().unwrap().port();
-	let server = run(listener).expect("Failed to bind address");
-	let _ = tokio::spawn(server);
-	// We return the application port to the caller.
-	let finalport = format!("http://127.0.0.1:{}", port);
-    println!("Server running on {}", finalport);
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
+    // reasons. This binding may cause issues in some environments. For example,
+    // it causes connectivity issues running in WSL2, where you cannot reach the
+    // server when it is bound to WSL2's localhost interface. As a workaround,
+    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
+    // of the security implications when you expose the server on all interfaces.
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
